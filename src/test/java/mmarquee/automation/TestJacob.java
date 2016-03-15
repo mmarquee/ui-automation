ea8c27b4254941c29b936a14504fb697710b4cba
Mark Humphreys <inpwtepydjuf@gmail.com>
2016-03-15 21:54:00 +0000

    ENH(Try and get jacon working, pt2)

--- src/test/java/mmarquee/automation/TestJacob.java
+++ src/test/java/mmarquee/automation/TestJacob.java
@@ -9,7 +9,8 @@ import com.jacob.com.Dispatch;
 public class TestJacob {
     public void run() {
 
-        ActiveXComponent msui = new ActiveXComponent("{30CBE57D-D9D0-452A-AB13-7AC5AC4825EE}");
+        ActiveXComponent msui =
+                new ActiveXComponent("{30CBE57D-D9D0-452A-AB13-7AC5AC4825EE}");
         Dispatch uiautomation = msui.getObject();
 
         Object root[] = new Object[1];
