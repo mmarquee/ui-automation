c390d4b3b5527466c6a81da971cb5ea6210641cc
Mark Humphreys <inpwtepydjuf@gmail.com>
2017-02-12 10:28:07 +0000

    ENH(Initial work on the Element under point, and element in focus)

---
+++ src/main/java/mmarquee/demo/DemoPointOver.java
@@ -0,0 +1,47 @@
+package mmarquee.demo;
+
+import com.sun.jna.platform.win32.WinDef;
+import mmarquee.automation.AutomationElement;
+import mmarquee.automation.UIAutomation;
+import mmarquee.automation.controls.AutomationApplication;
+import mmarquee.automation.controls.mouse.AutomationMouse;
+import mmarquee.automation.uiautomation.IUIAutomationElement3;
+
+import java.awt.*;
+
+/**
+ * Created by Mark Humphreys on 12/02/2017.
+ */
+public class DemoPointOver extends TestBase {
+    public DemoPointOver() {
+
+    }
+
+    public void run() {
+        UIAutomation automation = UIAutomation.getInstance();
+
+        do {
+            this.rest();
+
+            Point p = MouseInfo.getPointerInfo().getLocation();
+
+            logger.info(p.getX() + " - " + p.getY());
+
+            WinDef.POINT point = new WinDef.POINT();
+            point.x = (int) p.getX();
+            point.y = (int) p.getY();
+
+            try {
+                AutomationElement elementUnder = automation.getElementFromPoint(point);
+
+                logger.info("From point = " + elementUnder.getName());
+
+                AutomationElement elementFocus = automation.getFocusedElement();
+                logger.info("From focus = " + elementFocus.getName());
+
+            } catch (Exception ex) {
+                logger.info(ex.getStackTrace());
+            }
+        } while (true);
+    }
+}
---
+++ src/main/java/mmarquee/demo/PointerOver.java
@@ -0,0 +1,12 @@
+package mmarquee.demo;
+
+/**
+ * Created by Mark Humphreys on 12/02/2017.
+ */
+public class PointerOver {
+    public static void main(String[] args) {
+        DemoPointOver main = new DemoPointOver();
+
+        main.run();
+    }
+}
