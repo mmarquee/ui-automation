69db4d5b271382679157bfef448b0935f7b721c6
Pascal Bihler <pascal@qfs.de>
2017-05-05 11:34:31 +0200

    ENH(Refactored DemoTreeWalker to demonstrate simple recursion)

--- src/main/java/mmarquee/demo/DemoTreeWalker.java
+++ src/main/java/mmarquee/demo/DemoTreeWalker.java
@@ -5,13 +5,14 @@ import mmarquee.automation.AutomationException;
 import mmarquee.automation.AutomationTreeWalker;
 import mmarquee.automation.AutomationTreeWalker.AutomationElementVisitor;
 import mmarquee.automation.UIAutomation;
-import mmarquee.automation.controls.AutomationApplication;
 
 /**
  * Created by Mark Humphreys on 02/02/2017.
  */
 public class DemoTreeWalker extends TestBase {
 
+    final int recurse_level = 2;
+    
     public DemoTreeWalker() {
 
     }
@@ -19,22 +20,30 @@ public class DemoTreeWalker extends TestBase {
     public void run() {
         UIAutomation automation = UIAutomation.getInstance();
 
-        AutomationApplication application = null;
-
         try {
-            // Start the application
-            application = automation.launchOrAttach("explorer");
-
             AutomationTreeWalker walker = automation.getControlViewWalker();
 
             AutomationElement root = automation.getRootElement();
 
             AutomationElementVisitor logVisitor = new AutomationElementVisitor() {			
-				@Override
+				
+            	int level = 0;
+            	
+            	@Override
 				public boolean visit(AutomationTreeWalker walker, AutomationElement element) throws AutomationException {
 
-				    logger.info(element.getName());
-					logger.info(element.getClassName());
+				    String name = element.getName();
+					String className = element.getClassName();
+					String indent = level == 0 ? "" : String.format("%"+ level*2 + "s","");
+					String message = String.format("%s'%s' [%s]", indent, name, className);
+					
+					logger.info(message);
+					
+					if (recurse_level > level) {
+						level++;
+						walker.walk(this, element);
+						level--;
+					}
 					
 					return true;
 				}
