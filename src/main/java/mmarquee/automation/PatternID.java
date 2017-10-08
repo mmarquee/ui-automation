889842ff553d4e56298d5ae07346576a93d4da15
Mark Humphreys <inpwtepydjuf@gmail.com>
2017-10-08 20:36:47 +0100

    DOC(Further documentation)

--- src/main/java/mmarquee/automation/PatternID.java
+++ src/main/java/mmarquee/automation/PatternID.java
@@ -24,39 +24,175 @@ package mmarquee.automation;
  */
 
 public enum PatternID {
+
+    /**
+     * Invoke pattern.
+     */
     Invoke(10000),
+
+    /**
+     * Selection pattern.
+     */
     Selection(10001),
+
+    /**
+     * Value pattern.
+     */
     Value(10002),
+
+    /**
+     * RangeValue pattern.
+     */
     RangeValue(10003),
+
+    /**
+     * Scroll pattern.
+     */
     Scroll(10004),
+
+    /**
+     * ExpandCollapse pattern.
+     */
     ExpandCollapse(10005),
+
+    /**
+     * Grid pattern.
+     */
     Grid(10006),
+
+    /**
+     * GirdItem pattern.
+     */
     GridItem(10007),
+
+    /**
+     * MultipleView pattern.
+     */
     MultipleView(10008),
+
+    /**
+     * Window pattern.
+     */
     Window(10009),
+
+    /**
+     * SelectionItem pattern.
+     */
     SelectionItem(10010),
+
+    /**
+     * Dock pattern.
+     */
     Dock(10011),
+
+    /**
+     * Table pattern.
+     */
     Table(10012),
+
+    /**
+     * TableItem pattern.
+     */
     TableItem(10013),
+
+    /**
+     * Text pattern.
+     */
     Text(10014),
+
+    /**
+     * Toggle pattern.
+     */
     Toggle(10015),
+
+    /**
+     * Transform pattern.
+     */
     Transform(10016),
+
+    /**
+     * ScrollItem pattern.
+     */
     ScrollItem(10017),
+
+    /**
+     * LegacyAccessible pattern.
+     */
     LegacyIAccessible(10018),
+
+    /**
+     * ItemContainer pattern.
+     */
     ItemContainer(10019),
+
+    /**
+     * VirtualizedItem pattern.
+     */
     VirtualizedItem(10020),
+
+    /**
+     * SynchronizedInput patternn.
+     */
     SynchronizedInput(10021),
+
+    /**
+     * ObjectModel pattern.
+     */
     ObjectModel(10022),
+
+    /**
+     * Annotation pattern.
+     */
     Annotation(10023),
-    TextPattern2Id(10024),
+
+    /**
+     * Text2 pattern.
+     */
+    Text2(10024),
+
+    /**
+     * Styles pattern.
+     */
     Styles(10025),
+
+    /**
+     * Spreadsheet pattern.
+     */
     Spreadsheet(10026),
+
+    /**
+     * SpreadsheetItem pattern.
+     */
     SpreadsheetItem(10027),
-    TransformPattern2Id(10028),
+
+    /**
+     * Transform2 pattern.
+     */
+    Transform2(10028),
+
+    /**
+     * TextChild pattern.
+     */
     TextChild(10029),
+
+    /**
+     * Drag pattern.
+     */
     Drag(10030),
+
+    /**
+     * DropTarget pattern.
+     */
     DropTarget(10031),
+
+    /**
+     * TextEdit pattern.
+     */
     TextEdit(10032),
+
+    /**
+     * CustomNavigation pattern.
+     */
     CustomNavigation(10033);
 
     private int value;
@@ -65,6 +201,11 @@ public enum PatternID {
         return this.value;
     }
 
+    /**
+     * Constructor for PatterID.
+     *
+     * @param value Initial value.
+     */
     PatternID (int value) {
         this.value = value;
     }
