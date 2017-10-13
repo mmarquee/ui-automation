b7512ec0f12431023f36bc2c7195c62af11da9dd
Mark <inpwtepydjuf@gmail.com>
2017-10-13 22:21:43 +0100

    FIX(Make another eample work as expected, i.e. finish)

--- src/main/java/mmarquee/demo/TestMain.java
+++ src/main/java/mmarquee/demo/TestMain.java
@@ -58,248 +58,253 @@ public class TestMain extends TestBase {
     public void run() {
         UIAutomation automation = UIAutomation.getInstance();
 
-        AutomationApplication application = null;
-
         try {
-            application = automation.launchOrAttach("apps\\Project1.exe");
-        } catch (Throwable ex) {
-            logger.warn("Failed to find application", ex);
-        }
 
-        try {
-            // Wait for the process to start
-            application.waitForInputIdle(AutomationApplication.SHORT_TIMEOUT);
-        } catch (Throwable ex) {
-            logger.error("Failed to wait properly");
-        }
-
-        try {
-            AutomationWindow window = automation.getDesktopWindow("Form1");
-            String name = window.getName();
-            logger.info(name);
-
-            Object framework = window.getFramework();
-            logger.info("Framework is " + framework.toString());
-
-            logger.info("Model? :" + window.isModal());
-
-            java.lang.Object rect = window.getBoundingRectangle();
-
-            // Interact with menus
-            AutomationMainMenu menu = window.getMainMenu();
+            AutomationApplication application = null;
 
             try {
-                AutomationMenuItem exit = menu.getMenuItem("File", "Exit");
-                exit.click();
+                application = automation.launchOrAttach("apps\\Project1.exe");
+            } catch (Throwable ex) {
+                logger.warn("Failed to find application", ex);
+            }
+
+            try {
+                // Wait for the process to start
+                application.waitForInputIdle(AutomationApplication.SHORT_TIMEOUT);
+            } catch (Throwable ex) {
+                logger.error("Failed to wait properly");
+            }
+
+            try {
+                AutomationWindow window = automation.getDesktopWindow("Form1");
+                String name = window.getName();
+                logger.info(name);
+
+                Object framework = window.getFramework();
+                logger.info("Framework is " + framework.toString());
+
+                logger.info("Model? :" + window.isModal());
+
+                java.lang.Object rect = window.getBoundingRectangle();
+
+                // Interact with menus
+                AutomationMainMenu menu = window.getMainMenu();
 
                 try {
-                    AutomationWindow popup = window.getWindow("Project1");
-                    Object val111 = popup.getBoundingRectangle();
+                    AutomationMenuItem exit = menu.getMenuItem("File", "Exit");
+                    exit.click();
 
-                    AutomationButton btn = popup.getButton("OK");
-                    Object val11 = btn.getBoundingRectangle();
+                    try {
+                        AutomationWindow popup = window.getWindow("Project1");
+                        Object val111 = popup.getBoundingRectangle();
 
-                    boolean val1 = popup.isModal();
+                        AutomationButton btn = popup.getButton("OK");
+                        Object val11 = btn.getBoundingRectangle();
 
-                    btn.click();
-                } catch (ItemNotFoundException ex) {
-                    logger.info("Failed to find window");
+                        boolean val1 = popup.isModal();
+
+                        btn.click();
+                    } catch (ItemNotFoundException ex) {
+                        logger.info("Failed to find window");
+                    }
+                } catch (ElementNotFoundException ex) {
+                    logger.info("Failed to find menu");
                 }
-            } catch (ElementNotFoundException ex) {
-                logger.info("Failed to find menu");
-            }
 
-            AutomationTab tab = window.getTab(0);
-            tab.selectTabPage("Last Tab");
-            //	String tabName = tab.name();
+                AutomationTab tab = window.getTab(0);
+                tab.selectTabPage("Last Tab");
+                //	String tabName = tab.name();
 
-            String text = tab.getEditBox(0).getValue();
-            logger.info("Text for editBox1 is " + text);
+                String text = tab.getEditBox(0).getValue();
+                logger.info("Text for editBox1 is " + text);
 
-            AutomationCheckBox check = window.getCheckBox(0);
-            check.toggle();
+                AutomationCheckBox check = window.getCheckBox(0);
+                check.toggle();
 
-            try {
-                ToggleState state = check.getToggleState();
-                logger.info("State: " + state);
-            } catch (Exception ex) {
-                logger.info("Failed to get toggle state");
-            }
+                try {
+                    ToggleState state = check.getToggleState();
+                    logger.info("State: " + state);
+                } catch (Exception ex) {
+                    logger.info("Failed to get toggle state");
+                }
 
-            AutomationRadioButton radio = window.getRadioButton(1);
-            radio.select();
+                AutomationRadioButton radio = window.getRadioButton(1);
+                radio.select();
 
-            AutomationStatusBar statusBar = window.getStatusBar();
+                AutomationStatusBar statusBar = window.getStatusBar();
 
-            AutomationTextBox tb1 = statusBar.getTextBox(1);
+                AutomationTextBox tb1 = statusBar.getTextBox(1);
 
-            String eb1Text = tb1.getValue();
+                String eb1Text = tb1.getValue();
 
-            logger.info("Status Bar text = " + eb1Text);
+                logger.info("Status Bar text = " + eb1Text);
 
-            try {
-                AutomationComboBox cb1 = window.getCombobox("AutomatedCombobox1");
-                cb1.setText("Replacements");
-                String txt = cb1.getValue();
+                try {
+                    AutomationComboBox cb1 = window.getCombobox("AutomatedCombobox1");
+                    cb1.setText("Replacements");
+                    String txt = cb1.getValue();
 
-                cb1.getClickablePoint();
+                    cb1.getClickablePoint();
 
-                logger.info("Text for AutomatedCombobox1 is " + txt);
-            } catch (ElementNotFoundException ex) {
-                logger.error("Failed to find element");
-            }
+                    logger.info("Text for AutomatedCombobox1 is " + txt);
+                } catch (ElementNotFoundException ex) {
+                    logger.error("Failed to find element");
+                }
 
-            try {
-                AutomationMaskedEdit me0 = window.getMaskedEdit("AutomatedMaskEdit1");
+                try {
+                    AutomationMaskedEdit me0 = window.getMaskedEdit("AutomatedMaskEdit1");
 
-                String value = me0.getValue();
-                logger.info("Initial value " + value);
+                    String value = me0.getValue();
+                    logger.info("Initial value " + value);
 
-                me0.setValue("12/12/99");
+                    me0.setValue("12/12/99");
 
-                String value1 = me0.getValue();
-                logger.info("Changed value is " + value1);
+                    String value1 = me0.getValue();
+                    logger.info("Changed value is " + value1);
 
-            } catch (ElementNotFoundException ex) {
-                logger.error("Failed to find maskededit");
-            }
+                } catch (ElementNotFoundException ex) {
+                    logger.error("Failed to find maskededit");
+                }
 
-            try {
-                AutomationComboBox cb0 = window.getCombobox("AutomatedCombobox2");
-                cb0.expand();
+                try {
+                    AutomationComboBox cb0 = window.getCombobox("AutomatedCombobox2");
+                    cb0.expand();
 
-                this.rest();
+                    this.rest();
 
-                List<AutomationListItem> litems = cb0.getItems();
-            } catch (ElementNotFoundException ex) {
-                logger.error("Failed to find combobox");
-            }
+                    List<AutomationListItem> litems = cb0.getItems();
+                } catch (ElementNotFoundException ex) {
+                    logger.error("Failed to find combobox");
+                }
 
-            // Now string grids
-            AutomationDataGrid grid = window.getDataGrid("AutomationStringGrid1", "TAutomationStringGrid");
+                // Now string grids
+                AutomationDataGrid grid = window.getDataGrid("AutomationStringGrid1", "TAutomationStringGrid");
 
-            AutomationDataGridCell cell1 = grid.getItem(1, 1);
+                AutomationDataGridCell cell1 = grid.getItem(1, 1);
 
-            String itemName = cell1.getValue();
-            logger.info("Grid item is " + itemName);
+                String itemName = cell1.getValue();
+                logger.info("Grid item is " + itemName);
 //            cell1.setName("This");
 //            logger.info("Grid item is " + cell1.name());
 
-            AutomationTreeView tree = window.getTreeView(0);
-            try {
-                AutomationTreeViewItem treeItem = tree.getItem("Sub-SubItem");
-                treeItem.select();
-            } catch (ItemNotFoundException ex) {
-                // Not found
-            } catch (ElementNotFoundException ex) {
-                // Not found
-            }
-
-            AutomationList list = window.getList(0);
-            try {
-                AutomationListItem listItem = list.getItem("First (List)");
-                listItem.select();
-            } catch (ItemNotFoundException ex) {
-                // Not found
-            } catch (ElementNotFoundException ex) {
-                // Not found
-            }
-
-            try {
-                List<AutomationListItem> items = list.getItems();
-
-                for(AutomationListItem item: items) {
-                    logger.info(" *" +item.getName());
+                AutomationTreeView tree = window.getTreeView(0);
+                try {
+                    AutomationTreeViewItem treeItem = tree.getItem("Sub-SubItem");
+                    treeItem.select();
+                } catch (ItemNotFoundException ex) {
+                    // Not found
+                } catch (ElementNotFoundException ex) {
+                    // Not found
                 }
 
-            } catch (AutomationException ex) {
-                // Not found
-            }
-
-            AutomationHyperlink link = window.getHyperlink(0);
-            link.click();
-
-            try {
-                AutomationWindow popup1 = window.getWindow("Project1");
+                AutomationList list = window.getList(0);
                 try {
-                    AutomationButton btn1 = popup1.getButton("OK");
+                    AutomationListItem listItem = list.getItem("First (List)");
+                    listItem.select();
+                } catch (ItemNotFoundException ex) {
+                    // Not found
+                } catch (ElementNotFoundException ex) {
+                    // Not found
+                }
+
+                try {
+                    List<AutomationListItem> items = list.getItems();
+
+                    for (AutomationListItem item : items) {
+                        logger.info(" *" + item.getName());
+                    }
+
+                } catch (AutomationException ex) {
+                    // Not found
+                }
+
+                AutomationHyperlink link = window.getHyperlink(0);
+                link.click();
+
+                try {
+                    AutomationWindow popup1 = window.getWindow("Project1");
+                    try {
+                        AutomationButton btn1 = popup1.getButton("OK");
+                        btn1.click();
+                    } catch (ElementNotFoundException ex) {
+                        logger.info("Failed to find button");
+                    }
+                } catch (ItemNotFoundException ex) {
+                    logger.info("Failed to find window");
+                }
+
+                this.rest();
+
+                AutomationToolBar toolbar = window.getToolBar(1);
+                logger.info(toolbar.getName());
+
+                // Looks like the button is a problem with Delphi
+                AutomationToolBarButton btn0 = toolbar.getToolbarButton(0);
+
+                if (btn0.isEnabled()) {
+                    logger.info("btn0 Enabled");
+
+                    btn0.click();
+
+                    AutomationWindow popup1 = window.getWindow("Project1");
+                    try {
+                        AutomationButton btn1 = popup1.getButton("OK");
+                        btn1.click();
+                    } catch (ElementNotFoundException ex) {
+                        logger.info("Failed to find button");
+                    }
+                }
+
+                AutomationToolBarButton btn1 = toolbar.getToolbarButton(1);
+
+                if (btn1.isEnabled()) {
+                    logger.info("btn1 Enabled");
                     btn1.click();
-                } catch (ElementNotFoundException ex) {
-                    logger.info("Failed to find button");
+
+                    AutomationWindow popup1 = window.getWindow("Project1");
+                    try {
+                        AutomationButton btnOK = popup1.getButton("OK");
+                        btnOK.click();
+                    } catch (ElementNotFoundException ex) {
+                        logger.info("Failed to find button");
+                    }
                 }
-            } catch (ItemNotFoundException ex) {
-                logger.info("Failed to find window");
-            }
 
-            this.rest();
+                AutomationToolBarButton btn2 = toolbar.getToolbarButton(2);
 
-            AutomationToolBar toolbar = window.getToolBar(1);
-            logger.info(toolbar.getName());
-
-            // Looks like the button is a problem with Delphi
-            AutomationToolBarButton btn0 = toolbar.getToolbarButton(0);
-
-            if (btn0.isEnabled()) {
-                logger.info("btn0 Enabled");
-
-                btn0.click();
-
-                AutomationWindow popup1 = window.getWindow("Project1");
-                try {
-                    AutomationButton btn1 = popup1.getButton("OK");
-                    btn1.click();
-                } catch (ElementNotFoundException ex) {
-                    logger.info("Failed to find button");
+                if (btn2.isEnabled()) {
+                    logger.info("btn2 Enabled");
+                    btn2.click();
                 }
-            }
 
-            AutomationToolBarButton btn1 = toolbar.getToolbarButton(1);
+                AutomationToolBarButton btn3 = toolbar.getToolbarButton(3);
 
-            if (btn1.isEnabled()) {
-                logger.info("btn1 Enabled");
-                btn1.click();
-
-                AutomationWindow popup1 = window.getWindow("Project1");
-                try {
-                    AutomationButton btnOK = popup1.getButton("OK");
-                    btnOK.click();
-                } catch (ElementNotFoundException ex) {
-                    logger.info("Failed to find button");
+                if (btn3.isEnabled()) {
+                    logger.info("btn3 Enabled");
+                    btn3.click();
                 }
+
+                window.setTransparency(128);
+
+                // Context menus
+                WinDef.POINT point = grid.getClickablePoint();
+                AutomationMouse mouse = AutomationMouse.getInstance();
+
+                mouse.setLocation(point);
+                mouse.leftClick();
+                mouse.rightClick();
+
+                AutomationMenu context = automation.getDesktopMenu("Context");
+
+                logger.info("Found context menu");
+                AutomationMenuItem contextItem = context.getMenuItem("Popup Menu ");
+                contextItem.click();
+
+            } catch (Exception ex) {
+                logger.info("Something went wrong - " + ex.getClass());
             }
-
-            AutomationToolBarButton btn2 = toolbar.getToolbarButton(2);
-
-            if (btn2.isEnabled()) {
-                logger.info("btn2 Enabled");
-                btn2.click();
-            }
-
-            AutomationToolBarButton btn3 = toolbar.getToolbarButton(3);
-
-            if (btn3.isEnabled()) {
-                logger.info("btn3 Enabled");
-                btn3.click();
-            }
-
-            window.setTransparency(128);
-
-            // Context menus
-            WinDef.POINT point = grid.getClickablePoint();
-            AutomationMouse mouse = AutomationMouse.getInstance();
-
-            mouse.setLocation(point);
-            mouse.leftClick();
-            mouse.rightClick();
-
-            AutomationMenu context = automation.getDesktopMenu("Context");
-
-            logger.info("Found context menu");
-            AutomationMenuItem contextItem = context.getMenuItem("Popup Menu ");
-            contextItem.click();
-
-        } catch (Exception ex) {
-            logger.info("Something went wrong - " + ex.getClass());
+        } finally {
+            automation.cleanUp();
         }
     }
 }
