f5f1aabed7f0373cdbe7b58b56c07dae2b7ac398
mnmarquee <inpwtepydjuf@gmail.com>
2018-02-15 17:26:10 +0000

    ENH(Start to add Condition classe definitions)

---
+++ src/main/java/mmarquee/automation/controls/conditions/AndCondition.java
@@ -0,0 +1,10 @@
+package mmarquee.automation.controls.conditions;
+
+import com.sun.jna.ptr.PointerByReference;
+
+public class AndCondition extends Condition {
+
+    public AndCondition(PointerByReference inCondition) {
+        super(inCondition);
+    }
+}
